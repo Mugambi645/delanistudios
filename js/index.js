2f5b1553dfc51ec45e2338bf2985e50046f9b700
mugambi645 <mugambi645@gmail.com>
2021-09-18 17:13:22 +0300

    validation

--- js/index.js
+++ js/index.js
@@ -2,4 +2,25 @@ $(document).ready(function() {
     $('#btnInfo').bind('click', function( event ){
        alert('Creating Idealistic Designs');
     });
- });
+});
+$(document).ready(function() {
+ $('form[id="contact"]').validate({
+    rules: {
+      name: 'required',
+      message: 'required',
+      email: {
+        required: true,
+        email: true,
+      },
+
+    },
+    messages: {
+      name: 'This field is required',
+      email: 'Enter a valid email',
+      message: 'Message field cannot be empty',
+     
+    },
+    submitHandler: function(form) {
+      form.submit();
+    }
+  })});
