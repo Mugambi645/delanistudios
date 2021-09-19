8b298a7e8a6990653ae73dc5bdd9c467f23e0b67
mugambi645 <mugambi645@gmail.com>
2021-09-19 16:50:47 +0300

    scrolling effects

--- js/index.js
+++ js/index.js
@@ -27,4 +27,22 @@ $(document).ready(function() {
       form.submit();
     }
   })});
-  
+    
+ //back to top
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active')
+        } else {
+          backtotop.classList.remove('active')
+        }
+      }
+      window.addEventListener('load', toggleBacktotop)
+      onscroll(document, toggleBacktotop)
+    }
+  //back to top mouse over
+  let x = 0;
+  function backToTopOver() {
+    document.getElementById("arrow-right").innerHTML = "Back to top";
+  }
